974e66e7371be8dc2d0f223b6192b95326d83f58
Sonu Ram <sonuram6960@gmail.com>
2020-08-23 23:43:28 +0530

    Solve capitalizeFirst, capitalizeWords, nestedEvenSum and stringifyNumbers problems

--- problems/recursion/flatten.js
+++ problems/recursion/flatten.js
@@ -1,3 +1,15 @@
+/* Direction
+
+Write a function - flatten which accepts an array. The function should return a
+new array which is flattened version of the input array.
+
+Examples
+flatten([1, 2])             [1, 2]
+flatten([[1]])              [1]
+flatten([1, [2, 3], 4])     [1, 2, 3, 4]
+
+ */
+
 function flatten(arr, index = 0) {
   if (index === arr.length) return [];
 
