21a29563fe5937fa52b454629643ab1f1e962506
Sonu Ram <sonuram6960@gmail.com>
2020-09-19 22:03:48 +0530

    Solve problem - stack

---
+++ problems/data-structures/stackWithLinkedList.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    const node = new Node(val);
+
+    node.next = this.head;
+    this.head = node;
+    this.size++;
+
+    return node;
+  }
+
+  pop() {
+    if (!this.head) return null;
+
+    const remove = this.head;
+
+    this.head = this.head.next;
+    remove.next = null;
+    this.size--;
+
+    return remove;
+  }
+}
+
+const stack = new Stack();
+
+stack.push("hello");
+stack.push("there");
+stack.push("sonu");
+
+console.log(stack);
