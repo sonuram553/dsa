3845d95be1e8f3249dce6389f35962df14a5e3c3
Sonu Ram <sonuram6960@gmail.com>
2021-01-10 18:26:51 +0530

    Delete power, productOfArray and sum solutions

--- problems/recursion/productOfArray.js
+++
@@ -1,7 +0,0 @@
-function productOfArray(arr, index = 0) {
-  if (arr.length === 0) return null;
-  if (index === arr.length) return 1;
-  return arr[index] * productOfArray(arr, index + 1);
-}
-
-console.log(productOfArray([10]));
--- problems/recursion/sum.js
+++
@@ -1,6 +0,0 @@
-function sum(num) {
-  if (num === 0) return 0;
-  return num + sum(num - 1);
-}
-
-console.log(sum(5));
