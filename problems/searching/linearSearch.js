37ec4411652d04e3c4139017a4b54fbed769d010
Sonu Ram <sonuram6960@gmail.com>
2021-01-17 13:47:01 +0530

    Delete linearSearch solution

--- problems/searching/linearSearch.js
+++
@@ -1,9 +0,0 @@
-function linearSearch(arr, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === value) return i;
-  }
-
-  return -1;
-}
-
-console.log(linearSearch([1, 2, 3], 3));
