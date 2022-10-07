f263195cbc7d2d150838c78104472b4f77c2383a
Sonu Ram <sonuram6960@gmail.com>
2022-10-08 02:34:45 +0530

    Count Rotation using Binary Search

---
+++ array/rotation/count-rotation-2.js
@@ -0,0 +1,8 @@
+// Using Binary Search
+// Time Complexity: O(log N)
+
+import findIndexOfMin from "./find-min-in-rotated-sorted-array";
+
+function countRotation(arr) {
+  return findIndexOfMin(arr);
+}
