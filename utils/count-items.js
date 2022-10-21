ad39d42cdde743e9857bace9f133084b04046080
Sonu Ram <sonuram6960@gmail.com>
2022-10-21 20:32:29 +0530

    Count Items function

---
+++ utils/count-items.js
@@ -0,0 +1,12 @@
+function countItems(arr) {
+  const map = {};
+
+  for (const item of arr) {
+    if (map[item]) map[item] += 1;
+    else map[item] = 1;
+  }
+
+  return map;
+}
+
+export default countItems;
