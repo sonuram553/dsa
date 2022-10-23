88dac07d1ee9dff95e7a615f39445ca816e259b0
Sonu Ram <sonuram6960@gmail.com>
2022-10-23 22:14:03 +0530

    Add methods to deleteKey and removeMax

--- heap/max-heap.js
+++ heap/max-heap.js
@@ -3,6 +3,14 @@ import swap from "../utils/swap.js";
 class MaxHeap {
   arr = [];
 
+  get size() {
+    return this.arr.length;
+  }
+
+  getMax() {
+    return this.arr[0];
+  }
+
   insertKey(key) {
     let i = this.arr.length;
     this.arr.push(key);
@@ -11,6 +19,16 @@ class MaxHeap {
       swap(this.arr, parent(i), i);
       i = parent(i);
     }
+  }
+
+  // In order to delete a key
+  // at a given index i.
+  deleteKey(i) {
+    // It increases the value of the key
+    // to MAX Value and then removes
+    // the maximum value.
+    increaseKey(i, Number.MAX_VALUE);
+    removeMax();
   }
 
   // Increases value of key at
@@ -24,6 +42,32 @@ class MaxHeap {
     while (i !== 0 && this.arr[parent(i)] < this.arr[i]) {
       swap(this.arr, parent(i), i);
       i = parent(i);
+    }
+  }
+
+  removeMax() {
+    if (this.size === 0) return;
+    if (this.size === 1) return this.arr.pop();
+
+    const max = this.arr[0];
+    this.arr[0] = this.arr.pop();
+
+    this.heapify(0);
+    return max;
+  }
+
+  heapify(i) {
+    let left = leftChild(i);
+    let right = rightChild(i);
+    let largest = i;
+
+    if (left < this.size && this.arr[left] > this.arr[i]) largest = left;
+    if (right < this.size && this.arr[right] > this.arr[largest])
+      largest = right;
+
+    if (largest !== i) {
+      swap(this.arr, largest, i);
+      this.heapify(largest);
     }
   }
 }
