5c6194701338ceb0856a0471561ea54acf917744
Sonu Ram <sonuram6960@gmail.com>
2022-10-30 21:22:15 +0530

    Create dequeue method

--- priority-queue/priority-queue.js
+++ priority-queue/priority-queue.js
@@ -22,10 +22,39 @@ class PriorityQueue {
     this.bubbleUpI(this.arr.length - 1);
   }
 
+  dequeue() {
+    if (this.size === 0) return;
+    if (this.size === 1) return this.arr.pop();
+
+    const priorNode = this.arr[0];
+    this.arr[0] = this.arr.pop();
+
+    this.heapifyIterative(0);
+    return priorNode;
+  }
+
   bubbleUp(i) {
     while (i !== 0 && this.arr[parent(i)].priority > this.arr[i].priority) {
       swap(this.arr, parent(i), i);
       i = parent(i);
+    }
+  }
+
+  heapifyIterative(i) {
+    let largest = i;
+
+    while (true) {
+      const left = leftChild(i);
+      const right = rightChild(i);
+
+      if (left < this.size && this.arr[left] > this.arr[i]) largest = left;
+      if (right < this.size && this.arr[right] > this.arr[largest])
+        largest = right;
+
+      if (largest === i) break;
+
+      swap(this.arr, largest, i);
+      i = largest;
     }
   }
 }
