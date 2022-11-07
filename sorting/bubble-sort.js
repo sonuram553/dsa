1317bdbd298335d753496e3654b81c2968a8122c
Sonu Ram <sonuram6960@gmail.com>
2022-11-07 23:49:24 +0530

    Implement Bubble Sort

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,23 @@
+import swap from "../utils/swap.js";
+
+function bubbleSort(arr) {
+  let swapped;
+  let indexOfLastUnsortedItem = arr.length - 1;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i <= indexOfLastUnsortedItem; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
+    }
+
+    indexOfLastUnsortedItem--;
+  } while (swapped);
+
+  return arr;
+}
+
+export default bubbleSort;
